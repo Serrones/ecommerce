a5a85b03d4e74b8fcef90ebc220a86ec482bdcf4
serrones <fabioserrones@gmail.com>
2017-12-07 08:13:34 -0200

    Section 2 - Django Forms

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -6,6 +6,10 @@ Here we have a simple function based view (home_page)
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
+
+
 # Function based view that calls a html document from templates folder
 def home_page(request):
     context = {
@@ -24,15 +28,19 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
     "title": "Just look to the skies",
-    "content": "Welcome to the Contact Page"
+    "content": "Welcome to the Contact Page",
+    "form": contact_form
     }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
 
